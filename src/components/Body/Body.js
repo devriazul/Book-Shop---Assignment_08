ee3f3cd3130ba0762da804b80336fc3464de0ec6
devriazul <engr.riazul@gmail.com>
2022-03-26 16:11:47 +0600

    Books Added

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,29 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import Books from '../Books/Books';
-import Cart from '../Cart/Cart';
-import './Body.css'
+import React, { useEffect, useState } from "react";
+import Books from "../Books/Books";
+import Cart from "../Cart/Cart";
+import "./Body.css";
 const Body = () => {
-    //load books from json
-    const [books, setBooks] = useState([]);
-    useEffect(()=>{
-        fetch('books.json')
-        .then(res=>res.json())
-        .then(data=>setBooks(data))
-    },[]);
-    // console.log(books);
 
-    //add to cart function
-    const [cart, setCart] = useState([]);
-    const addToCart = cartInfo =>{
-        setCart([...cart,cartInfo]);
+  //load books from json
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+  // console.log(books);
+
+  //add to cart function
+  const [cart, setCart] = useState([]);
+  const addToCart = (cartInfo) => {
+    if (cart.length < 4) {
+      setCart([...cart, cartInfo]);
     }
-    console.log(cart);
-    return (
-        <div className='body'>
-            <Books books={books} addToCart={addToCart}></Books>
-            <Cart insideCart={cart}></Cart>
-        </div>
-    );
+  };
+
+  // clear cart function
+  const clearCart = () => {
+    setCart([]);
+  };
+  
+  return (
+    <div className="body">
+      <Books books={books} addToCart={addToCart}></Books>
+      <Cart insideCart={cart} clearCart={clearCart}></Cart>
+    </div>
+  );
 };
 
 export default Body;
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import CartInfo from "../CartInfo/CartInfo";
 import "./Cart.css";
 const Cart = (props) => {
-  const { insideCart } = props;
+  //destructuring props
+  const { insideCart, clearCart} = props;
+  
+  //create random
+  const [random, setRandom] = useState(0);
+  const choseRandomly = () => {
+    setRandom(Math.floor(Math.random() * insideCart.length));
+
+    //when cart is empty
+    if (insideCart[0] === undefined) {
+      setRandomBook("");
+    }
+
+    //when cart have at least one product
+    if (insideCart[0]) {
+      setRandomBook(insideCart[random].name);
+    }
+  };
+  
+  //show random book name to ui
+  const [randomBook, setRandomBook] = useState("");
   return (
     <div className="cart">
-      <h3>Selected Item: {insideCart.length}</h3>
-      { insideCart.map((aCart) => <CartInfo key={aCart.id} aCart={aCart} ></CartInfo>)}
+      <h3>Selected Item: {insideCart.length} Out of 4</h3>
+      {insideCart.map((aCart) => (
+        <CartInfo key={Math.random()} aCart={aCart}></CartInfo>
+      ))}
+      <h3>Randomly Chosen</h3>
+      <p className="random-book-name">{randomBook}</p>
+      
+      <div className="btn-group">
+        <button onClick={() => choseRandomly()} className="btn-random">
+          Chose Randomly <i className="fa fa-random" aria-hidden="true"></i>
+        </button>
+        <button onClick={() => clearCart()} className="btn-clear">
+          Clear Cart <i className="fa fa-times-circle-o" aria-hidden="true"></i>
+        </button>
+      </div>
     </div>
   );
 };
